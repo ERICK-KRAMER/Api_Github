5b37996b1a401078c465249acdb857eb17bf629c
Kramer <kramerfilho@hotmail.com>
2023-09-26 22:00:26 -0300

    implementaçao de busca

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,51 +2,57 @@ import React, { useEffect, useState } from "react";
 import style from './Modal.module.css'
 import { AiOutlineClose } from 'react-icons/ai'
 import axios from "axios";
-export function Modal({ isOpen, setIsOpenModal, getName}) {
 
-    //https://api.github.com/users/${name}
-
-    const [data, setData] = useState([]);
+export function Modal({ isOpen, setIsOpenModal, getName }) {
+    const [data, setData] = useState({});
     const [error, setError] = useState(null);
     const [imageProfile, setImageProfile] = useState('');
+    const [repos, setRepos] = useState([])
 
     async function getData() {
         try {
-            const response = await axios.get(`https://api.github.com/users/${getName}`)
+            const response = await axios.get(`https://api.github.com/users/${getName}`);
             setData(response.data);
-            setImageProfile(response.data.avatar_url)
-            console.log(data)
+            setImageProfile(response.data.avatar_url);
+            setRepos(response.data.repos_url)
+            // console.log(repos)
+            // console.log(data);
+            // console.log(imageProfile);
         } catch (error) {
-            setError('algo deu errado')
+            setError('Algo deu errado');
         }
     }
-    useEffect(() => {
-     getData();   
-    },[data])
 
-    if(isOpen) {
-        return(
+    useEffect(() => {
+        if (isOpen && getName) {
+            getData();
+        }
+    }, [isOpen, getName]);
+
+    if (isOpen) {
+        return (
             <div className={style.background}>
-               <div className={style.modal}>
-                <button onClick={() => {setIsOpenModal(!isOpen)}}><AiOutlineClose /></button>
-                    {data.length > 0 && (
-                        <>
-                        {data.map((item) => (
-                            <>
-                            <img src={imageProfile} alt={data.login} />
-                            <p>Repositorios:</p>
-                              <ul>
-                                {}
-                              </ul>
-                            </>
-                        ))}
-                        </>
+                <div className={style.modal}>
+                    <button onClick={() => { setIsOpenModal(!isOpen) }}><AiOutlineClose /></button>
+                    {data.login && (
+                        <div className={style.profile}>
+                            <img src={imageProfile} alt={data.name} />
+                            <span>{data.login}</span>
+                            <ul>
+                                {/* {repos.map((item)=>(
+                                    <li key={item.id}>{item.full_name}</li>
+                                ))} */}
+                            </ul>
+                        </div>
                     )}
+
                     {error && (
                         <div>{error}</div>
                     )}
-               </div>
+                </div>
             </div>
         );
+    } else {
+        return null;
     }
 }
